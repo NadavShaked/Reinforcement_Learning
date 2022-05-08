d66e69c46034b3b2a2faa3446b560f0c9df0cd47
nadavshaked <nadav2sh@gmail.com>
2022-05-08 21:04:12 +0300

    update

--- Middle project/Solution/maze.py
+++ Middle project/Solution/maze.py
@@ -111,7 +111,7 @@ class MazeEnv(gym.Env):
             reward = 1
             done = True
         else:
-            reward = -0.1 / (self.maze_size[0] * self.maze_size[1]) # todo: update here
+            reward = -0.1 / (self.maze_size[0] * self.maze_size[1])
             done = False
 
         self.state = self.maze_view.robot
